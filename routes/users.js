0ef8b595c5ec1beddc91472c211ebd6efea375aa
Jamie Ruderman <jruderman@enphaseenergy.com>
2015-03-24 10:49:36 -0700

    Improved system actions menus

--- routes/users.js
+++ routes/users.js
@@ -21,16 +21,30 @@ router.get('/:id', function(req, res, next) {
 /* Single User Summary */
 router.get('/:id/systems', function(req, res, next) {
   var index = req.params.id,
-      user = users[index];
+      user = users[index],
+      params = { 
+        title: user.name, 
+        user: user, 
+        index: index, 
+        actions: [{
+          name: 'System summary',
+          path: 'systems'
+        }, {
+          name: 'Energy lifetime',
+          path: 'lifetime'
+        }] 
+      };
   
-  var api = new Enphase({
+  new Enphase({
     api: 'systems',
     user: user,
     success: function(data) {
-      res.render('systems', { title: user.name, user: user, index: index, data: data });
+      params.data = data;
+      res.render('systems', params);
     },
     error: function(data) {
-      res.render('api_error', { title: user.name, user: user, index: index, data: data });
+      params.data = data;
+      res.render('api_error', params);
     }
   });
 });
@@ -40,7 +54,7 @@ router.get('/:id/systems/:system', function(req, res, next) {
   var index = req.params.id,
       user = users[index];
 
-  var api = new Enphase({
+  new Enphase({
     api: 'summary',
     system: req.params.system,
     user: user,
